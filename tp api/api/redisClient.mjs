514e04ca95317011fe38943a5257b8528cd50670
Mostapha <mustapha8moukrim@gmail.com>
2024-10-15 15:11:32 +0200

    docker compose

--- tp api/api/redisClient.mjs
+++ tp api/api/redisClient.mjs
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-// const redisUrl = process.env.REDIS_URL
+//const redisUrl = process.env.REDIS_URL
 
 const client = await createClient({ url: process.env.REDIS_URL })
   .on("error", (error) => console.log(error))
