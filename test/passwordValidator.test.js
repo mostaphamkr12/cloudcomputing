23e3a2484928462cb172ce07c3face9abd6011d6
Mostapha <mustapha8moukrim@gmail.com>
2024-11-05 17:16:04 +0100

    test

--- test/passwordValidator.test.js
+++ test/passwordValidator.test.js
@@ -1,16 +1,6 @@
-
-
-const  {validatePassword}= require('./passwordValidatorV1.js');
-
-
+const { validatePassword } = require('./passwordValidatorV1.js');
 
 test('Password should have at least 8 characters', () => {
-    var password = "12ee345%6";
+    const password = "12eA35%6";
     expect(validatePassword(password)).toBe(true);
-}
-);
-
-test('Password should be valid', () => {
-    const password = "aZ1*bcde";
-    assert.strictEqual(validatePassword(password), true);
 });
